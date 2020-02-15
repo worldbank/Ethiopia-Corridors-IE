ff05a228b96ab367659126d48d8912e727b27448
Rob Marty <ramarty@email.wm.edu>
2020-02-15 13:09:18 -0500

    prep full grid

--- Code/_ethiopia_ie_master.R
+++ Code/_ethiopia_ie_master.R
@@ -6,6 +6,8 @@ if(Sys.info()[["user"]] == "WB554990") project_file_path <- "C:/Users/wb521633/D
 if(Sys.info()[["user"]] == "WB521633") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/IEs/Ethiopia IE"
 if(Sys.info()[["user"]] == "robmarty") project_file_path <- "~/Dropbox/World Bank/IEs/Ethiopia IE"
 
+if(Sys.info()[["user"]] == "robmarty") code_file_path <- "~/Documents/Github/Ethiopia-Corridors-IE/Code"
+
 rawdata_file_path <- file.path(project_file_path, "Data", "RawData")
 outputs_for_grid <- file.path(project_file_path, "Data", "IntermediateData", "Outputs for Grid")
 finaldata_file_path <- file.path(project_file_path, "Data", "FinalData")
@@ -14,8 +16,10 @@ tables_file_path <- file.path(project_file_path,"Outputs", "Results", "Tables")
 
 # Parameters -------------------------------------------------------------------
 
+# dmspols_grid_dataset_nearroad
 #DATASET_TYPE <- "dmspols_grid_dataset_randomsample"
-DATASET_TYPE <- "woreda_panel_hdx_csa"
+#DATASET_TYPE <- "woreda_panel_hdx_csa"
+DATASET_TYPE <- "dmspols_grid_dataset_nearroad"
 
 if(DATASET_TYPE %in% c("woreda_panel_hdx_csa")){
   CHUNK_SIZE_DIST_ROADS <- 3
@@ -59,6 +63,30 @@ library(ggpubr)
 library(readr)
 library(gdistance)
 source("https://raw.githubusercontent.com/ramarty/fast-functions/master/R/functions_in_chunks.R")
+
+# Run Scripts ------------------------------------------------------------------
+##### Extract Data to Grids
+grid_scripts <- c("02a_extract_gadm.R", 
+                  "02b_extract_ndvi.R",
+                  "02b_extract_viirs.R",
+                  "02c_extract_distance_improved_roads_by_speedlimit_after.R",
+                  "02c_extract_distance_improved_roads_by_speedlimit_before.R",
+                  "02c_extract_distance_roads_by_phase.R",
+                  "02c_extract_distance_roads_by_speedlimit.R",
+                  "02d_distance_cities.R",
+                  "02e_extract_globcover.R",
+                  "02f_extract_dmspols_intercalibrated_zhang2016_method.R",
+                  "04b_merge_datasets_dmspols.R", 
+                  "05_create_variables_for_analysis.R")
+for(script_i in grid_scripts){
+  print(paste(script_i, "----------------------------------------------------"))
+  source(file.path(code_file_path, "create_clean_datasets", "02_create_gridded_datasets", script_i))
+} 
+  
+
+
+
+
 
 
 
---
+++ Code/create_clean_datasets/02_create_gridded_datasets/01_create_points_dmspols_fullsampleroadsarea.R
@@ -0,0 +1,109 @@
+# Create Points at DMSPOLS Level
+
+# Creates a blank (1) point dataset and (2) polygon dataset of grids at the DMSP-OLS
+# level (ie, 1x1km grid). These datasets contain a unique ID and spatial information.
+# The points file is saved as a dataframe with lat/lon while the poylgon is saved
+# as a spatial features object.
+
+# The script also generates a dataset that includes a panel of DMSP-OLS data. It 
+# is done in this script as the process for preparing the above files makes
+# creating this one quick
+
+# NOTE: This script takes about 20 minutes to run.
+
+set.seed(42)
+
+PROPORTION_SAMPLE <- 0.05
+
+# Load Data --------------------------------------------------------------------
+dmspols <- raster(file.path(rawdata_file_path, "Nighttime Lights", "DMSP_OLS","Stacked", "eth_dmspols_allyears.tif"),1)
+roads <- readRDS(file.path(project_file_path, "Data", "FinalData", "roads", "RoadNetworkPanelData_1996_2016.Rds"))
+eth_adm3 <- readRDS(file.path(rawdata_file_path, "GADM", "gadm36_ETH_3_sp.rds"))
+
+# Determine Pixels to Keep Based on Location -----------------------------------
+# Creates a vector of cells_to_keep that is TRUE when we should keep cells. Don't
+# directly restrict cells yet as using raster format. Subsetting comes in later
+# step after polygonizing and making point file.
+
+#### Restrict to Ethiopia Based on GEE
+# Only keep cells without value for NTL. Removes area not in Ethiopia according to GEE;
+# this is because GEE only exported NTL for areas in the country.
+dmspols_df <- dmspols[] %>% as.data.frame
+cells_to_keep <- !is.na(dmspols_df$.)
+
+#### Restrict to Ethiopia Based on GADM
+# Only keep cells within boundary of Ethiopia according to GADM
+dmspols_coords <- coordinates(dmspols) %>% 
+  as.data.frame %>%
+  dplyr::rename(long = x) %>%
+  dplyr::rename(lat = y) %>%
+  mutate(id = 1:n())
+coordinates(dmspols_coords) <- ~long+lat
+crs(dmspols_coords) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+
+points_OVER_gadm <- sp::over(dmspols_coords, eth_adm3)
+
+cells_to_keep <- cells_to_keep & !is.na(points_OVER_gadm$NAME_0)
+
+#### Restrict to Near Road
+# Restrict roads to consider
+roads <- roads[roads$Complete_G >= 1997,]
+
+# Simplify roads to make buffering go faster
+roads_s <- gSimplify(roads, tol=.001)
+roads_s$id <- 1:length(roads_s)
+roads_buff <- gBuffer_chunks(roads_s, width=7/111.12, 50)
+roads_buff$id <- 1
+roads_buff_agg <- raster::aggregate(roads_buff, by="id")
+
+points_OVER_road <- over_chunks(dmspols_coords, roads_buff_agg, "sum", 20000)
+
+cells_to_keep <- cells_to_keep & !is.na(points_OVER_road$id)
+
+# Keep Random Sample of Cells --------------------------------------------------
+# We restrict the sample to an X percent sample. We further refine the cells_to_keep
+# vector.
+
+#cells_to_keep[cells_to_keep %in% TRUE] <- sample(x = c(TRUE, FALSE),
+#                                                 size = length(cells_to_keep[cells_to_keep %in% TRUE]),
+#                                                 replace = T,
+#                                                 prob = c(PROPORTION_SAMPLE, 1-PROPORTION_SAMPLE))
+
+# Individual Points ------------------------------------------------------------
+dmspols_coords <- coordinates(dmspols) %>% 
+  as.data.frame %>%
+  dplyr::rename(long = x) %>%
+  dplyr::rename(lat = y)
+dmspols_coords <- dmspols_coords[cells_to_keep,]
+dmspols_coords$cell_id <- 1:nrow(dmspols_coords)
+
+saveRDS(dmspols_coords, file.path(finaldata_file_path, "dmspols_grid_dataset_nearroad", "individual_datasets","points.Rds"))
+write_csv(dmspols_coords, file.path(finaldata_file_path, "dmspols_grid_dataset_nearroad", "individual_datasets","points.csv"))
+
+# Polygon of Points ------------------------------------------------------------
+dmspols_poly <- polygonize(dmspols, na.rm=F)
+dmspols_poly <- dmspols_poly[cells_to_keep,]
+dmspols_poly$cell_id <- 1:nrow(dmspols_poly)
+dmspols_poly$eth_dmspols_allyears <- NULL
+
+saveRDS(dmspols_poly, file.path(finaldata_file_path, "dmspols_grid_dataset_nearroad", "individual_datasets","polygons.Rds"))
+
+# DMSP Panel -------------------------------------------------------------------
+# Create datsaet of DMSPOLS data. Easier to do here because if the cells_to_keep
+# variable
+dmspols_panel <- lapply(1:22, function(i){
+  print(i)
+  dmspols_i <- raster(file.path(rawdata_file_path, "Nighttime Lights", "DMSP_OLS","Stacked", "eth_dmspols_allyears.tif"),i)[] %>% 
+    as.data.frame %>%
+    dplyr::rename(dmspols = ".")
+  dmspols_i$year <- i + 1991
+  
+  dmspols_i <- dmspols_i[cells_to_keep,]
+  dmspols_i$cell_id <- 1:nrow(dmspols_i)
+  
+  return(dmspols_i)
+}) %>%
+  bind_rows
+
+saveRDS(dmspols_panel, file.path(finaldata_file_path, "dmspols_grid_dataset_nearroad", "individual_datasets","points_dmspols.Rds"))
+
