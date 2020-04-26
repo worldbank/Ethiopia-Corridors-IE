5b2e1069a193eda6377aba8ef4ea2a333af9fea7
Rob Marty <ramarty@email.wm.edu>
2020-04-25 21:42:57 -0400

    annote cleaning script

--- Code/01_create_clean_datasets/02_create_analysis_datasets/05_merge_and_clean_data/04c_merge_datasets_dmspols_nearroad.R
+++ Code/01_create_clean_datasets/02_create_analysis_datasets/05_merge_and_clean_data/04c_merge_datasets_dmspols_nearroad.R
@@ -10,17 +10,11 @@ points_all <- readRDS(file.path(finaldata_file_path, DATASET_TYPE,"individual_da
 #### Names of datasets to merge in
 # Separate by time invarient (merge by cell_id) and time varying (merge by cell_id 
 # and year)
-DATASETS_TIME_INVARIANT <- c("points_gadm.Rds", "points_distance_cities.Rds", "points_distance_rsdp_phases.Rds",
+DATASETS_TIME_INVARIANT <- c("points_gadm.Rds", 
+                             "points_distance_cities.Rds", 
+                             "points_distance_rsdp_phases.Rds",
                              "points_distance_anyroad2016.Rds")
-#DATASETS_TIME_INVARIANT <- c("points_gadm.Rds", "points_distance_cities.Rds")
 
-#DATASETS_TIME_VARYING <- c("points_viirs.Rds",
-#                           "points_ndvi.Rds",
-#                           "points_globcover.Rds",
-#                           "points_dmspols_zhang2016.Rds",
-#                           "points_distance_roads_byspeed.Rds",
-#                           "points_distance_improved_roads_byspeed_before.Rds",
-#                           "points_distance_improved_roads_byspeed_after.Rds")
 DATASETS_TIME_VARYING <- c("points_globcover.Rds",
                            "points_ndvi.Rds",
                            "points_dmspols_zhang2016.Rds",
--- Code/_ethiopia_ie_master.R
+++ Code/_ethiopia_ie_master.R
@@ -1,6 +1,9 @@
 # Ethiopia IE 
 # Master R Script
 
+# In order to source a script as a local job, include this at top of script
+# source("~/Documents/Github/Ethiopia-Corridors-IE/Code/_ethiopia_ie_master.R")
+
 # Filepaths --------------------------------------------------------------------
 if(Sys.info()[["user"]] == "WB554990") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/IEs/Ethiopia IE - Merge Budget Data With Shapefile"
 if(Sys.info()[["user"]] == "WB521633") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/IEs/Ethiopia IE"
@@ -14,16 +17,27 @@ finaldata_file_path <- file.path(project_file_path, "Data", "FinalData")
 figures_file_path <- file.path(project_file_path,"Outputs", "Results", "Figures")
 tables_file_path <- file.path(project_file_path,"Outputs", "Results", "Tables")
 
-# In order to source a script as a local job, include this at top of script
-# source("~/Documents/Github/Ethiopia-Corridors-IE/Code/_ethiopia_ie_master.R")
-
 # Parameters -------------------------------------------------------------------
 
-# dmspols_grid_dataset_nearroad
-#DATASET_TYPE <- "dmspols_grid_dataset_randomsample"
-DATASET_TYPE <- "woreda_panel_hdx_csa"
-#DATASET_TYPE <- "dmspols_grid_dataset_nearroad"
+#### DATASET
+# Defines dataset to run analysis on. Either at woreda level, grid level, or
+# grid subsample:
+# OPTIONS:
+# --"dmspols_grid_dataset_nearroad": DMSP-OLS level dataset restricted to cells
+#                                    near a road
+# --"dmspols_grid_dataset_randomsample": DMSP-OLS level dataset; random sample
+# --"woreda_panel_hdx_csa": Woreda level
 
+#DATASET_TYPE <- "dmspols_grid_dataset_randomsample"
+#DATASET_TYPE <- "woreda_panel_hdx_csa"
+DATASET_TYPE <- "dmspols_grid_dataset_nearroad"
+
+#### CHUNK SIZE
+# For some functions, we break up the dataset into chunks. These are vectorized
+# functions; however, vectorizing across the whole sample (eg, 1km grid across
+# all of Ethiopia) would take up too much memory. Consequently, we vectorize
+# into manageable chunks. Chunk size differs depending on grid level or woreda
+# level.
 if(DATASET_TYPE %in% c("woreda_panel_hdx_csa")){
   CHUNK_SIZE_DIST_ROADS <- 3
 } else{
@@ -31,7 +45,6 @@ if(DATASET_TYPE %in% c("woreda_panel_hdx_csa")){
 }
 
 # Parameters for Grid Analysis
-
 MCCORS_DIST_ROADS <- 1
 TYPE <- c("DMSPOLS") # globcover, DMSPOLS
 UTM_ETH <- '+init=epsg:20138'
@@ -71,22 +84,22 @@ source("https://raw.githubusercontent.com/ramarty/fast-functions/master/R/functi
 # Run Scripts ------------------------------------------------------------------
 ##### Extract Data to Grids
 if(F){
-grid_scripts <- c("02a_extract_gadm.R", 
-                  #"02b_extract_ndvi.R",
-                  #"02b_extract_viirs.R",
-                  "02c_extract_distance_improved_roads_by_speedlimit_after.R",
-                  #"02c_extract_distance_improved_roads_by_speedlimit_before.R",
-                  #"02c_extract_distance_roads_by_phase.R",
-                  #"02c_extract_distance_roads_by_speedlimit.R",
-                  "02d_distance_cities.R",
-                  "02e_extract_globcover.R",
-                  "02f_extract_dmspols_intercalibrated_zhang2016_method.R",
-                  "04b_merge_datasets_dmspols.R", 
-                  "05_create_variables_for_analysis.R")
-for(script_i in grid_scripts){
-  print(paste(script_i, "----------------------------------------------------"))
-  source(file.path(code_file_path, "create_clean_datasets", "02_create_gridded_datasets", script_i))
-} 
+  grid_scripts <- c("02a_extract_gadm.R", 
+                    #"02b_extract_ndvi.R",
+                    #"02b_extract_viirs.R",
+                    "02c_extract_distance_improved_roads_by_speedlimit_after.R",
+                    #"02c_extract_distance_improved_roads_by_speedlimit_before.R",
+                    #"02c_extract_distance_roads_by_phase.R",
+                    #"02c_extract_distance_roads_by_speedlimit.R",
+                    "02d_distance_cities.R",
+                    "02e_extract_globcover.R",
+                    "02f_extract_dmspols_intercalibrated_zhang2016_method.R",
+                    "04b_merge_datasets_dmspols.R", 
+                    "05_create_variables_for_analysis.R")
+  for(script_i in grid_scripts){
+    print(paste(script_i, "----------------------------------------------------"))
+    source(file.path(code_file_path, "create_clean_datasets", "02_create_gridded_datasets", script_i))
+  } 
 }
 
 
