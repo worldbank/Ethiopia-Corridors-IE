fcd63505281f0c5f94cfd3cfe9dd050ca73a9237
Rob Marty <ramarty@email.wm.edu>
2020-05-12 19:20:06 -0400

    first diff models

--- Code/02_rsdp_impact_analysis/03_analysis_ward_specific/long_difference/long_difference.R
+++ Code/02_rsdp_impact_analysis/03_analysis_ward_specific/long_difference/long_difference.R
@@ -5,6 +5,18 @@ control_vars <- "+ temp_avg + precipitation"
 # Load Data --------------------------------------------------------------------
 data <- readRDS(file.path(finaldata_file_path, DATASET_TYPE, "merged_datasets", "grid_data_clean_all.Rds"))
 
+#### Add variable
+data$road_length_X_speed <- data$road_length_10 * 10 + 
+  data$road_length_15 * 15 + 
+  data$road_length_20 * 20 + 
+  data$road_length_25 * 25 +
+  data$road_length_30 * 30 +
+  data$road_length_35 * 35 +
+  data$road_length_45 * 45 + 
+  data$road_length_50 * 50 + 
+  data$road_length_70 * 70 + 
+  data$road_length_120 * 120 
+
 data_dmspols <- data %>%
   filter(year %in% c(1996, 2012)) %>%
   mutate(endline = as.numeric(year %in% 2012)) 
@@ -18,18 +30,6 @@ data_full <- data %>%
   mutate(endline = as.numeric(year %in% 2016)) 
 
 results_df <- data.frame(NULL)
-
-#### Add variable
-data$road_length_X_speed <- data$road_length_10 * 10 + 
-  data$road_length_15 * 15 + 
-  data$road_length_20 * 20 + 
-  data$road_length_25 * 25 +
-  data$road_length_30 * 30 +
-  data$road_length_35 * 35 +
-  data$road_length_45 * 45 + 
-  data$road_length_50 * 50 + 
-  data$road_length_70 * 70 + 
-  data$road_length_120 * 120 
 
 counter <- 1
 for(dv in c("viirs_mean_2",
@@ -56,6 +56,7 @@ for(dv in c("viirs_mean_2",
     for(iv_suffix in c("", "_area", "_neigh", "_neigh_area", "_neigh_withi", "_neigh_withi_area")){
       for(addis_dist in c("All", "Far")){
         
+        ## Grab dataset
         if (grepl("dmsp", dv)){
           data_temp <- data_dmspols
         } else if (grepl("viirs", dv)){
@@ -64,6 +65,7 @@ for(dv in c("viirs_mean_2",
           data_temp <- data_full
         }
         
+        ## Subset to Far from Addis
         if(addis_dist %in% "Far"){
           data_temp <- data_temp[data_temp$far_addis %in% 1,] 
         }
