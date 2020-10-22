31d9fefd9a009313a83d550eca8429da0ee4af8a
Rob Marty <ramarty@email.wm.edu>
2020-10-22 10:24:23 -0400

    parameters #17

--- _ethiopia_ie_master.R
+++ _ethiopia_ie_master.R
@@ -6,46 +6,18 @@
 
 # Parameters -------------------------------------------------------------------
 
-# Filepaths --------------------------------------------------------------------
-if(Sys.info()[["user"]] == "WB554990") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/IEs/Ethiopia IE - Merge Budget Data With Shapefile"
-if(Sys.info()[["user"]] == "WB521633") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/IEs/Ethiopia IE"
-if(Sys.info()[["user"]] == "robmarty") project_file_path <- "~/Dropbox/World Bank/IEs/Ethiopia IE"
-if(Sys.info()[["user"]] == "andm2") project_file_path <- "C:/Users/andm2/Dropbox/WorldBank/Ethiopia IE"
-
-if(Sys.info()[["user"]] == "robmarty") code_file_path <- "~/Documents/Github/Ethiopia-Corridors-IE"
-if(Sys.info()[["user"]] == "andm2") code_file_path <- "G:/Work/Ethiopia/Ethiopia-Corridors-IE"
-
-rawdata_file_path <- file.path(project_file_path, "Data", "RawData")
-outputs_for_grid <- file.path(project_file_path, "Data", "IntermediateData", "Outputs for Grid")
-data_file_path <- file.path(project_file_path, "Data")
-panel_rsdp_imp_file_path <- file.path(project_file_path, "Data", "Panel Data RSDP Impacts")
-panel_rsdp_imp_data_file_path <- file.path(panel_rsdp_imp_file_path, "Data")
-#figures_file_path <- file.path(project_file_path,"Outputs", "Results", "Figures")
-#tables_file_path <- file.path(project_file_path,"Outputs", "Results", "Tables")
-
-rsdp_impact_code_file_path <- file.path(code_file_path, "DataWork", "Panel Data RSDP Impacts")
-rsdp_impact_prep_data_code_file_path <- file.path(code_file_path, "DataWork", "Panel Data RSDP Impacts", "01_create_main_analysis_datasets")
-
-# Parameters -------------------------------------------------------------------
-
-#### Ethiopia UTM
-UTM_ETH <- '+init=epsg:20138'
-
 #### DATASET
 # Defines dataset to run analysis on. Either at woreda level, grid level, or
 # grid subsample:
 # OPTIONS:
-# --"dmspols_grid_dataset_nearroad": DMSP-OLS level dataset restricted to cells
-#                                    near a road
-# --"dmspols_grid_dataset_randomsample": DMSP-OLS level dataset; random sample
-# --"woreda_panel_hdx_csa": Woreda level
-
-# NEW
-# --"dmspols_grid_nearroad"
-# --"dmspols_grid_nearroad_randomsample"
-# --"woreda"
-
+# --"dmspols_grid_nearroad": Near 10km of any road as of 2016
+# --"dmspols_grid_nearroad_randomsample": Random sample of above
+# --"woreda": Woreda polygons
 DATASET_TYPE <- "dmspols_grid_nearroad"
+
+# Some scripts check whether DATASET_TYPE is a grid or polygon (eg, woreda) level.
+# Inidates whether grid level for if/else statements for script
+GRID_DATASET <- grepl("grid", DATASET_TYPE)
 
 #### CHUNK SIZE
 # For some functions, we break up the dataset into chunks. These are vectorized
@@ -53,11 +25,10 @@ DATASET_TYPE <- "dmspols_grid_nearroad"
 # all of Ethiopia) would take up too much memory. Consequently, we vectorize
 # into manageable chunks. Chunk size differs depending on grid level or woreda
 # level.
-if(DATASET_TYPE %in% c("woreda",
-                       "woreda_panel_hdx_csa_nearroad")){
-  CHUNK_SIZE_DIST_ROADS <- 3
-} else{
+if(GRID_DATASET){
   CHUNK_SIZE_DIST_ROADS <- 1250
+} else{
+  CHUNK_SIZE_DIST_ROADS <- 3
 }
 
 #### YEAR SUBSETS
@@ -69,11 +40,39 @@ road_year <- list(all = 1996:2016,
                   phase3 = 2008:2010, # 2007:2010
                   phase4 = 2011:2016) # 2010:2015
 
-#### Other
-MCCORS_DIST_ROADS <- 1
-TYPE <- c("DMSPOLS") # globcover, DMSPOLS
-DIST_THRESH <- 2
-GRID_DATASET <- grepl("grid", DATASET_TYPE)
+# Some functions set up to use multiple cores (mclapply). Number of cores to use.
+MCCORS_DIST_ROADS <- 1 
+
+# Ethiopia UTM
+UTM_ETH <- '+init=epsg:20138' # Ethiopia UTM
+
+# Close to road threshold (kilometers)
+DIST_THRESH <- 2 
+
+#### RUN SCRIPT PARAMETERS
+CREATE_UNIT_LEVEL_DATASETS <- F
+
+EXTRACT_DATA <- T
+OVERWRITE_EXTRACTED_DATA <- T # Checks if data already extracted. If T, re-extracts
+# data. If F, skips extracting data
+
+# Filepaths --------------------------------------------------------------------
+if(Sys.info()[["user"]] == "WB554990") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/IEs/Ethiopia IE - Merge Budget Data With Shapefile"
+if(Sys.info()[["user"]] == "WB521633") project_file_path <- "C:/Users/wb521633/Dropbox/World Bank/IEs/Ethiopia IE"
+if(Sys.info()[["user"]] == "robmarty") project_file_path <- "~/Dropbox/World Bank/IEs/Ethiopia IE"
+if(Sys.info()[["user"]] == "andm2")    project_file_path <- "C:/Users/andm2/Dropbox/WorldBank/Ethiopia IE"
+
+if(Sys.info()[["user"]] == "robmarty") code_file_path <- "~/Documents/Github/Ethiopia-Corridors-IE"
+if(Sys.info()[["user"]] == "andm2")    code_file_path <- "G:/Work/Ethiopia/Ethiopia-Corridors-IE"
+
+data_file_path <- file.path(project_file_path, "Data")
+panel_rsdp_imp_file_path <- file.path(project_file_path, "Data", "Panel Data RSDP Impacts")
+panel_rsdp_imp_data_file_path <- file.path(panel_rsdp_imp_file_path, "Data")
+#figures_file_path <- file.path(project_file_path,"Outputs", "Results", "Figures")
+#tables_file_path <- file.path(project_file_path,"Outputs", "Results", "Tables")
+
+rsdp_impact_code_file_path <- file.path(code_file_path, "DataWork", "Panel Data RSDP Impacts")
+rsdp_impact_prep_data_code_file_path <- file.path(code_file_path, "DataWork", "Panel Data RSDP Impacts", "01_create_main_analysis_datasets")
 
 # Packages ---------------------------------------------------------------------
 library(AER)
@@ -115,11 +114,6 @@ source("https://raw.githubusercontent.com/ramarty/fast-functions/master/R/functi
 source(file.path(code_file_path, "Functions", "commonly_used.R"))
 
 # RUN SCRIPTS ==================================================================
-CREATE_UNIT_LEVEL_DATASETS <- F
-EXTRACT_DATA <- T
-OVERWRITE_EXTRACTED_DATA <- T # Checks if data already extracted. If T, re-extracts
-                              # data. If F, skips extracting data
-#MERGE_CLEAN_DATA <- F
 
 # ** Create Unit Level Datasets ------------------------------------------------
 if(CREATE_UNIT_LEVEL_DATASETS){
@@ -146,11 +140,11 @@ if(EXTRACT_DATA){
   ## Scripts specific to units
   if(GRID_DATASET){
     scripts_unit_specific <- file.path(rsdp_impact_prep_data_code_file_path, 
-                                   "02_extract_variables_grid_specific") %>%
+                                       "02_extract_variables_grid_specific") %>%
       list.files(pattern = ".R", full.names = T)
   } else{
     scripts_unit_specific <- file.path(rsdp_impact_prep_data_code_file_path, 
-                              "02_extract_variables_woreda_specific") %>%
+                                       "02_extract_variables_woreda_specific") %>%
       list.files(pattern = ".R", full.names = T)
   }
   
