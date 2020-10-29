39eb788200284215941f9d6e6aeb26ede012aa04
Rob Marty <ramarty@email.wm.edu>
2020-10-29 13:30:00 -0400

    start rad improvement map

---
+++ DataWork/RSDP Roads/figures/road_improvement_map.R
@@ -0,0 +1,58 @@
+# Road Improvement Map
+
+# Load Data --------------------------------------------------------------------
+roads <- readRDS(file.path(data_file_path, "RSDP Roads", "FinalData", "RoadNetworkPanelData_1996_2016.Rds"))
+eth_adm <- readRDS(file.path(data_file_path, "GADM", "RawData", "gadm36_ETH_0_sp.rds"))
+
+#roads <- roads[runif(nrow(roads)) < 0.1,]
+
+# Prep Data --------------------------------------------------------------------
+## Road completion year
+roads@data <- roads@data %>%
+  dplyr::rename(completion_year = Complete_G) %>%
+  dplyr::select(completion_year)
+
+roads_existing <- roads[roads$completion_year <= 1996,]
+roads_improved <- roads[roads$completion_year > 1996,]
+
+# roads$completion_year[roads$completion_year <= 1996] <- "No\nImprovement"
+# 
+# roads$completion_year <- roads$completion_year %>%
+#   factor(c("No\nImprovement", 1997:2016))
+
+## Tidy dataframe
+roads_improved$id <- row.names(roads_improved)
+roads_improved_tidy <- tidy(roads_improved)
+roads_improved_tidy <- merge(roads_improved_tidy, roads_improved@data, by = "id")
+
+## Factor 
+ggplot() +
+  geom_polygon(data = eth_adm,
+               aes(x = long, y = lat, group = group),
+               fill = NA, color = "white") +
+  geom_path(data = roads_existing,
+            aes(x = long, y = lat, group = group),
+            color = "gray") +
+  geom_path(data = roads_improved_tidy,
+            aes(x = long, y = lat, group = group, color = completion_year)) +
+  theme_void() +
+  scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "Spectral"))) +
+  labs(color = "Improvement\nYear") +
+  theme(plot.background = element_rect(fill = "black",
+                                       color = "black"),
+        legend.title = element_text(color = "white", hjust = 0.5),
+        legend.text = element_text(color = "white")) +
+  coord_quickmap() +
+  ggsave(file.path(data_file_path, "RSDP Roads", "Outputs", "figures",
+                   "road_improvement_map.png"),
+         height = 6,
+         width = 6)
+
+
+
+
+
+
+
+
+
