86062481333c7cc1704fd59c4164b87daa2af82e
Rob Marty <ramarty@email.wm.edu>
2020-11-16 21:25:18 -0500

    mst

--- DataWork/Hypothetical Road Networks/02_create_euclidean_distance.R
+++ DataWork/Hypothetical Road Networks/02_create_euclidean_distance.R
@@ -5,50 +5,46 @@
 woreda_points <- readRDS(file.path(data_file_path, "Hypothetical Road Networks", "points_to_connect.Rds"))
 
 ## Elevation/Slope
-elevation <- raster(file.path(data_file_path, "Elevation", "RawData", "gee_download", "eth_elevation_1000m.tif"))
-slope <- terrain(elevation, opt="slope", unit="degrees",neighbors=8)
-
-## Ethiopia ADM boundary
-# The "woreda" file has holes for water bodies
-eth <- readRDS(file.path(data_file_path, "GADM", "RawData", "gadm36_ETH_0_sp.rds")) %>%
-  gBuffer(width = 10/111.12)
+# elevation <- raster(file.path(data_file_path, "Elevation", "RawData", "gee_download", "eth_elevation_1000m.tif"))
+# slope <- terrain(elevation, opt="slope", unit="degrees",neighbors=8)
+# 
+# ## Ethiopia ADM boundary
+# # The "woreda" file has holes for water bodies
+# eth <- readRDS(file.path(data_file_path, "GADM", "RawData", "gadm36_ETH_0_sp.rds")) %>%
+#   gBuffer(width = 10/111.12)
 
 # Cost Surface -----------------------------------------------------------------
-cost_r <- slope
-cost_r[] <- 1
-cost_r <- cost_r %>% mask(eth)
+#cost_r <- slope
+#cost_r[] <- 1
+#cost_r <- cost_r %>% mask(eth)
 
 # Least Cost Path --------------------------------------------------------------
 # https://stackoverflow.com/questions/52601127/r-how-to-find-least-cost-path-through-raster-image
-cost_t <- transition(cost_r, function(x) 1/mean(x), directions=8)
+#cost_t <- transition(cost_r, function(x) 1/mean(x), directions=8)
 
 extract_path_cost <- function(i){
   
   print(i)
   
-  path_i <- shortestPath(cost_t, 
-                         woreda_points[i,], 
-                         woreda_points[(i+1):nrow(woreda_points),], 
-                         output = "SpatialLines")
+  path_i <- lapply(i:(nrow(woreda_points)-1), function(row){
+    #print(row)
+    
+    l <- rbind(woreda_points[i,],
+               woreda_points[(row+1),]) %>%
+      as("SpatialLines")
+    
+    l$cost <- gDistance(woreda_points[row,],
+                        woreda_points[(row+1),])
+    
+    return(l)
+    
+  }) %>% 
+    do.call(what = "rbind")
   
-  cost_i <- costDistance(cost_t, 
-                         woreda_points[i,], 
-                         woreda_points[(i+1):nrow(woreda_points),])
-  
-  #s <- woreda_points[-i,]
-  #s$cost <- cost_i %>% as.vector()
-  #plot(cost_r)
-  #plot(s[s$cost %in% Inf,], add=T)
-  
-  path_i$temp <- 1:length(path_i)
-  path_i$cost <- cost_i %>% as.vector()
   path_i$origin <- woreda_points$uid[i]
   path_i$dest <- woreda_points$uid[(i+1):nrow(woreda_points)]
   
-  #path_cost_i <- SpatialLinesDataFrame(sl = path_i, data = data.frame(cost=as.numeric(cost_i),
-  #                                                                    origin=markets_all_long_origin$origin[i],
-  #                                                                    dest=markets_all_long_origin$dest[i]), match.ID = FALSE)
-  return(path_i)
+   return(path_i)
 }
 
 least_cost_paths_sdf <- lapply(1:(nrow(woreda_points)-1), extract_path_cost) %>% do.call(what="rbind")
