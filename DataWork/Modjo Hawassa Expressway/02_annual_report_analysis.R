4123864c6b96c6f48430c797c571a044a4c24b17
Rob Marty <ramarty@email.wm.edu>
2021-04-15 23:11:16 -0400

    figureS

--- DataWork/Modjo Hawassa Expressway/02_annual_report_analysis.R
+++ DataWork/Modjo Hawassa Expressway/02_annual_report_analysis.R
@@ -28,16 +28,17 @@ compute_ntl_stats <- function(year, buffer){
 
 ntl_stats <- map_df(2012:2020, compute_ntl_stats, 10)
 
-ntl_stats %>%
+p_ntl_trends <- ntl_stats %>%
   ggplot(aes(x = year, y = mean)) +
   geom_line(size = 2,
-            color = "black") +
-  geom_point() +
+            color = "goldenrod2") +
+  #geom_point() +
   ylim(c(0, 0.5)) +
   labs(x = NULL,
        y = "Luminosity",
        title = "Average Luminosity within 10km of the\nModjo-Hawassa Expressway from 2012 - 2020") +
   theme_ipsum()
+
 
 # Maps -------------------------------------------------------------------------
 ## Buffer
@@ -64,6 +65,10 @@ viirs_20_mh_df <- viirs_20_mh %>%
   as.data.frame()
 viirs_20_mh_df$value <- viirs_20_mh[]
 viirs_20_mh_df$value_l <- log(viirs_20_mh_df$value+1)
+
+## Change
+viirs_20_mh_df$growth <- viirs_20_mh_df$value - viirs_15_mh_df$value
+viirs_20_mh_df$growth_l <- viirs_20_mh_df$value_l - viirs_15_mh_df$value_l
 
 ## Figures
 viirs_15_mh_df$value_l %>% summary()
@@ -107,8 +112,62 @@ p20 <- ggplot() +
         plot.title = element_text(hjust = 0.5, face = "bold")) +
   coord_quickmap()
 
-ggarrange(p15, p20)
+viirs_20_mh_df$growth_l[viirs_20_mh_df$growth_l > 1.5] <- 1.5
+viirs_20_mh_df$growth_l[viirs_20_mh_df$growth_l < -1.5] <- -1.5
 
+pchange <- ggplot() +
+  geom_raster(data = viirs_20_mh_df,
+              aes(x = x, y=y, fill = growth_l)) +
+  geom_polygon(data = mh_road_10km,
+               aes(x = long, y=lat, group = group),
+               fill = NA,
+               color = "white",
+               size = 0.1) +
+  scale_fill_gradient2(low = "mediumorchid1",
+                       mid = "black",
+                       high = "green",
+                       midpoint = 0,
+                       limits = c(-1.5, 1.5),
+                       labels = c("Decrease", "", "", 
+                                  "No Change",
+                                  "", "", "Increase")) +
+  labs(title = "Change in Nighttime Lights\nfrom 2015 to 2020",
+       fill = "Change\nin NTL") +
+  theme_void() +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  coord_quickmap()
 
+p_15_20 <- ggarrange(p15, p20,
+                     nrow = 1) %>%
+  annotate_figure(top = text_grob("Nighttime Lights within 10km of\nModjo-Hawassa Expressway", color = "black", face = "bold", size = 12, hjust = 0.5)
+  )
+
+# Export Figures ---------------------------------------------------------------
+ggsave(p_15_20,
+       filename = file.path(project_file_path, 
+                            "Data", 
+                            "Modjo Hawassa Expressway",
+                            "Outputs", 
+                            "ntl_2015_2020.png"),
+       height = 4.75,
+       width = 4)
+
+ggsave(pchange,
+       filename = file.path(project_file_path, 
+                            "Data", 
+                            "Modjo Hawassa Expressway",
+                            "Outputs", 
+                            "ntl_2015_2020_change.png"),
+       height = 4.75,
+       width = 3.5)
+
+ggsave(p_ntl_trends,
+       filename = file.path(project_file_path, 
+                            "Data", 
+                            "Modjo Hawassa Expressway",
+                            "Outputs", 
+                            "ntl_trends.png"),
+       height = 4,
+       width = 6)
 
 
